5af2dccfa2d2221f5656965a81d4d7279b2583f4
Adrian Hartmann <haadrian@student.ethz.ch>
2021-12-28 16:16:40 +0100

    2020 Day1

---
+++ 2020/1/report_repair.py
@@ -0,0 +1,42 @@
+#%%
+import numpy as np
+
+expense = np.genfromtxt("input.txt")
+
+
+def search(array, l, h, res):
+    while l < h and (s := array[l] + array[h]) != res:
+        if s < res:
+            l += 1
+        else:
+            h -= 1
+    return l, h
+
+
+# %% part 1
+expense = np.sort(expense)
+
+low, high = search(expense, 0, expense.shape[0] - 1, 2020)
+
+print(
+    f"Part 1 - Expenses: {expense[low]}*{expense[high]} = {expense[low]*expense[high]}"
+)
+
+# %% part 2
+
+pivot = expense.shape[0] - 1
+
+while True:
+    low = 0
+    high = pivot - 1
+    low, high = search(expense, 0, pivot - 1, 2020 - expense[pivot])
+
+    if expense[low] + expense[high] + expense[pivot] == 2020:
+        break
+    pivot -= 1
+
+print(
+    f"Part 2 - Expenses: {expense[pivot]}*{expense[low]}*{expense[high]} = {expense[pivot]*expense[low]*expense[high]}"
+)
+
+# %%
