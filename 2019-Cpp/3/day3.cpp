e9d35df6e9260bc357250be231aced419a021928
Adrian <haadrian@student.ethz.ch>
2022-03-03 20:59:58 +0100

    19 Day3 part a

--- 2019-Cpp/3/day3.cpp
+++ 2019-Cpp/3/day3.cpp
@@ -4,13 +4,14 @@
 #include <vector>
 #include <tuple>
 #include <set>
+#include <math.h>
 
 void getCommands(std::vector<std::tuple<char, int>> *commands, std::string input)
 {
     int start = 0;
     for (int i = 0; i < input.length(); i++)
     {
-        if (input[i] == ',')
+        if (input[i] == ',' || i == input.length() - 1)
         {
             char instr = input[start];
             int num = std::stoi(input.substr(start + 1, i - start));
@@ -20,27 +21,10 @@ void getCommands(std::vector<std::tuple<char, int>> *commands, std::string input
     }
 }
 
-int main()
+void insertLine(std::set<std::tuple<int, int>> *map, std::vector<std::tuple<char, int>> *commands)
 {
-    std::ifstream inputFile("../3/input.txt");
-
-    // input container
-    std::vector<std::tuple<char, int>> commands1;
-    std::vector<std::tuple<char, int>> commands2;
-
-    std::string line1;
-    std::string line2;
-
-    std::getline(inputFile, line1);
-    std::getline(inputFile, line2);
-
-    // parse input into <direction, distance> pairs
-    getCommands(&commands1, line1);
-    getCommands(&commands2, line2);
-
-    // walk over all positions and insert in set, save collisions
-    std::set<std::tuple<int, int>> map;
-    for (std::tuple<char, int> cmd : commands1)
+    int pos[2] = {0, 0};
+    for (std::tuple<char, int> cmd : *commands)
     {
         int dir[2] = {0, 0};
         switch (std::get<0>(cmd))
@@ -60,13 +44,59 @@ int main()
         default:
             std::cout << "Unknown Direction: " << std::get<0>(cmd);
         }
-        
+        int dist = std::get<1>(cmd);
         // work in progress
+        for (int i = 0; i < dist; i++)
+        {
+            pos[0] += dir[0];
+            pos[1] += dir[1];
+            auto res = map->emplace(std::make_tuple(pos[0], pos[1]));
+        }
     }
+}
 
-    int start = 0;
+int main()
+{
+    std::ifstream inputFile("../3/input.txt");
+
+    // input container
+    std::vector<std::tuple<char, int>> commands1;
+    std::vector<std::tuple<char, int>> commands2;
+
+    std::string line1;
+    std::string line2;
+
+    std::getline(inputFile, line1);
+    std::getline(inputFile, line2);
 
     inputFile.close();
+
+    // parse input into <direction, distance> pairs
+    getCommands(&commands1, line1);
+    getCommands(&commands2, line2);
+
+    // walk over all positions and insert in a set each
+    std::set<std::tuple<int, int>> map1;
+    std::set<std::tuple<int, int>> map2;
+    insertLine(&map1, &commands1);
+    insertLine(&map2, &commands2);
+
+    // merge both sets (avoids collisions between the same line)
+    int min_hattan = INT_MAX;
+    for (auto p : map1)
+    {
+        auto res = map2.emplace(p);
+        if (!res.second)
+        {
+            int d = std::abs(std::get<0>(*res.first)) + std::abs(std::get<1>(*res.first));
+            if (d < min_hattan)
+            {
+                min_hattan = d;
+                std::cout << d << "\n";
+            }
+        }
+    }
+    std::cout << "Smallest Distance: " << min_hattan;
 
     return 0;
 }
