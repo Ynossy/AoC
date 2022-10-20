9d2d20a5be6bdc44ebb8487783f129e879abcb53
Adrian <haadrian@student.ethz.ch>
2022-10-20 17:17:50 +0200

    Finish 18day02p2

--- 2018-Rust/day02/src/main.rs
+++ 2018-Rust/day02/src/main.rs
@@ -1,14 +1,13 @@
-use core::num::dec2flt::number;
 use std::fs;
 
 fn main() {
     let path = "input.txt";
     let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
-    let numbers = part1(&contents);
-    part2(&contents, &numbers);
+    part1(&contents);
+    part2(&contents);
 }
 
-fn part1(contents: &String) -> Vec<[i32; 26]> {
+fn part1(contents: &String) {
     let mut numbers = vec![[0; 26]; contents.len()];
     let mut two = 0;
     let mut three = 0;
@@ -38,7 +37,26 @@ fn part1(contents: &String) -> Vec<[i32; 26]> {
         three,
         two * three
     );
-    return numbers;
 }
 
-fn part2(contents: &String, numbers: Vec<[i32; 26]>) {}
+fn part2(contents: &String) {
+    let lines: Vec<&str> = contents.trim().lines().collect();
+    'task: for line1 in &lines {
+        for line2 in &lines {
+            let vec_line1: Vec<char> = line1.chars().collect();
+            let vec_line2: Vec<char> = line2.chars().collect();
+            let mut same = 0;
+            for c in 0..vec_line1.len() {
+                if vec_line1[c] == vec_line2[c] {
+                    same += 1;
+                }
+            }
+            if same == vec_line1.len()-1 {
+                println!("Found Strings: ");
+                println!("{}", line1);
+                println!("{}", line2);
+                break 'task;
+            }
+        }
+    }
+}
