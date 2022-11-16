859d09c0bc8686dc3d828772fbfc237f431c4c33
Adrian <haadrian@student.ethz.ch>
2022-11-16 11:33:58 +0100

    Add 19day11 part2

--- 2019-Cpp/11/day11.cpp
+++ 2019-Cpp/11/day11.cpp
@@ -39,14 +39,9 @@ struct Point
     };
 };
 
-void part1(std::vector<long> input)
-{
-    // 8548 too high
-    // 2428 correct
-    std::unordered_map<Point, long, Point::HashFunction> map;
-
+void run(std::vector<long> &input, std::unordered_map<Point, long, Point::HashFunction> &map, long start_color){
     IntCode pc(input);
-    pc.push_input(0);
+    pc.push_input(start_color);
     int x = 0;
     int y = 0;
     int dir = 0; // 0: UP, 1: RIGHT, 2: DOWN, 3: LEFT
@@ -88,8 +83,54 @@ void part1(std::vector<long> input)
     }
 }
 
+void part1(std::vector<long> input)
+{
+    // 8548 too high
+    // 2428 correct
+    std::unordered_map<Point, long, Point::HashFunction> map;
+    run(input, map, 0);
+}
+
 void part2(std::vector<long> input)
 {
+    // RJLFBUCU
+    // .###....##.#....####.###..#..#..##..#..#...
+    // .#..#....#.#....#....#..#.#..#.#..#.#..#...
+    // .#..#....#.#....###..###..#..#.#....#..#...
+    // .###.....#.#....#....#..#.#..#.#....#..#...
+    // .#.#..#..#.#....#....#..#.#..#.#..#.#..#...
+    // .#..#..##..####.#....###...##...##...##....
+    std::unordered_map<Point, long, Point::HashFunction> map;
+    run(input, map, 1);
+    int max_x = INT16_MIN;
+    int min_x = INT16_MAX;
+    int max_y = INT16_MIN;
+    int min_y = INT16_MAX;
+
+    for(auto p: map){
+        max_x = p.first.x > max_x ? p.first.x : max_x;
+        min_x = p.first.x < min_x ? p.first.x : min_x;
+        max_y = p.first.y > max_y ? p.first.y : max_y;
+        min_y = p.first.y < min_y ? p.first.y : min_y;
+    }
+    std::cout << max_x << " " << min_x << " " << max_y << " " << min_y << "\n";
+    int size_x = 1+max_x-min_x;
+    int size_y = 1+max_y-min_y;
+    std::vector<std::vector<long>> drawing(size_y, std::vector<long>(size_x));
+    for(auto p: map){
+        int x = p.first.x;
+        int y = p.first.y;
+        drawing.at(y-min_y).at(x-min_x) = p.second;
+    }
+
+    for(int i=size_y-1; i>=0; i--){
+        for(auto j:drawing.at(i)){
+            std::cout << (j ? '#' : '.');
+        }
+        std::cout << "\n";
+    }
+
+
 }
 
 int main()
@@ -114,6 +155,6 @@ int main()
     inputFile.close();
 
     part1(input);
-    // part2(input);
+    part2(input);
     return 0;
 }
