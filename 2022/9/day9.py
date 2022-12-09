f89ef6ea1e39152ec8362cd7635042b7186af260
Adrian <haadrian@student.ethz.ch>
2022-12-09 10:56:19 +0100

    22 day 9 part 2

--- 2022/9/day9.py
+++ 2022/9/day9.py
@@ -1,5 +1,28 @@
 import numpy as np
-dirs = {'U':[0,1], 'D':[0,-1], 'R':[1,0], 'L':[-1,0]}
+
+dirs = {"U": [0, 1], "D": [0, -1], "R": [1, 0], "L": [-1, 0]}
+
+
+def simulate_rope(moves, n):
+    position = set((0, 0))
+
+    rope = [np.array([0, 0]) for i in range(n)]
+
+    for move in moves:
+        for _ in range(move[1]):
+            rope[0] = np.add(rope[0], dirs[move[0]])
+            for i in range(n - 1):
+                if np.linalg.norm(rope[i] - rope[i + 1]) < 1.5:
+                    continue
+                if rope[i + 1][0] != rope[i][0]:
+                    rope[i + 1][0] += 1 if rope[i + 1][0] < rope[i][0] else -1
+                if rope[i + 1][1] != rope[i][1]:
+                    rope[i + 1][1] += 1 if rope[i + 1][1] < rope[i][1] else -1
+                if i + 1 == n - 1:
+                    position.add((rope[i + 1][0], rope[i + 1][1]))
+
+    print(f"Positons covered: {len(position)}")
+
 
 def main():
     with open("input.txt") as f:
@@ -7,24 +30,9 @@ def main():
             (s.split(" ")[0], int(s.split(" ")[1]))
             for s in f.read().strip().split("\n")
         ]
-    position = set((0,0))
 
-    H = np.array([0, 0])
-    T = np.array([0, 0])
-
-    for move in moves:
-        for _ in range(move[1]):
-            H = np.add(H, dirs[move[0]])
-            if np.linalg.norm(H-T) < 1.5:
-                continue
-            if T[0] != H[0]:
-                T[0] += 1 if T[0] < H[0] else -1
-            if T[1] != H[1]:
-                T[1] += 1 if T[1] < H[1] else -1
-            position.add((T[0],T[1]))
-
-    print(f"Positons covered: {len(position)}") # 5619
-
+    simulate_rope(moves, 2)  # 5619
+    simulate_rope(moves, 10)  # 2376
 
 
 if __name__ == "__main__":
