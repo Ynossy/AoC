73e93f0857f93d19226041a5b4719aa706ecd880
Adrian Ubuntudesk <adrian.hartmann@bluewin.ch>
2022-12-12 17:13:05 +0100

    22 day 12 part 2

--- 2022/12/day12.py
+++ 2022/12/day12.py
@@ -1,28 +1,12 @@
-def find_shortest():
-    pass
-
-def main():
-    with open("input.txt") as f:
-        heightmap = [[ord(i) - ord("a") for i in line] for line in f.read().strip().split("\n")]
+def find_shortest(start, end, heightmap):
     size_x = len(heightmap)
     size_y = len(heightmap[0])
-    start = [
-        (i, j) for i in range(size_x) for j in range(size_y) if heightmap[i][j] == -14
-    ][0]
-    start_a = [
-        (i, j) for i in range(size_x) for j in range(size_y) if heightmap[i][j] == 0
-    ]
-    end = [
-        (i, j) for i in range(size_x) for j in range(size_y) if heightmap[i][j] == -28
-    ][0]
-    heightmap[start[0]][start[1]] = 0
-    heightmap[end[0]][end[1]] = 25
     path_len = [[1e6 for i in range(size_y)] for j in range(size_x)]
     path_len[start[0]][start[1]] = 0
 
     stack = [start]
     dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-    while True:
+    while stack:
         pos = stack.pop(0)
         if pos[0] == end[0] and pos[1] == end[1]:
             break
@@ -36,8 +20,32 @@ def main():
                 ):
                     path_len[pos[0] + d[0]][pos[1] + d[1]] = value + 1
                     stack.append((pos[0] + d[0], pos[1] + d[1]))
+    return path_len[end[0]][end[1]]
 
-    print(path_len[end[0]][end[1]]) # 517
+
+def main():
+    with open("input.txt") as f:
+        heightmap = [
+            [ord(i) - ord("a") for i in line] for line in f.read().strip().split("\n")
+        ]
+    size_x = len(heightmap)
+    size_y = len(heightmap[0])
+    start = [
+        (i, j) for i in range(size_x) for j in range(size_y) if heightmap[i][j] == -14
+    ][0]
+    start_a = [
+        (i, j) for i in range(size_x) for j in range(size_y) if heightmap[i][j] == 0
+    ]
+    end = [
+        (i, j) for i in range(size_x) for j in range(size_y) if heightmap[i][j] == -28
+    ][0]
+    heightmap[start[0]][start[1]] = 0
+    heightmap[end[0]][end[1]] = 25
+
+    print(find_shortest(start, end, heightmap))  # 517
+
+    min_path = min([find_shortest(s, end, heightmap) for s in start_a])  # 512
+    print(min_path)
 
 
 if __name__ == "__main__":
