551e3a468167f3f649ba1a90756ace0229bc337c
Adrian <haadrian@student.ethz.ch>
2022-12-16 19:40:35 +0100

    22 day 16 part2 runs for an eternity, lol

--- 2022/16/day16.py
+++ 2022/16/day16.py
@@ -1,5 +1,6 @@
 import time
 
+
 def find_shortest_path(rooms, start, end):  # BFS
     stack = [start]
     dist = {name: 1000 for name in rooms.keys()}
@@ -18,11 +19,11 @@ def find_shortest_path(rooms, start, end):  # BFS
 def maximal_pressure(room, t, rooms, good_valves, path_len, open_valves):
     rate = sum([rooms[r][0] for r in good_valves if open_valves[r]])
     # print(room, rate, t)
-    if sum([x for x in open_valves.values()]) == len(good_valves):
+    if sum([x for x in open_valves.values()]) == len(open_valves):
         return rate * t
     J = []
     for next_valve in good_valves:
-        if open_valves[next_valve]==True:
+        if open_valves[next_valve] == True:
             continue
         o = open_valves.copy()
         o[next_valve] = True
@@ -40,6 +41,61 @@ def maximal_pressure(room, t, rooms, good_valves, path_len, open_valves):
             )
     J.append(rate * t)
     # print(J)
+    return max(J)
+
+
+def maximal_pressure2(room: tuple, t, dt, rooms, good_valves, path_len, open_valves):
+    # print(room)
+    rate = sum([rooms[r][0] for r in good_valves if open_valves[r]])
+    if sum([x for x in open_valves.values()]) == len(open_valves):
+        print("here")
+        return rate * t
+    J = []
+    for next_valve in good_valves:
+        if open_valves[next_valve] == True or next_valve in room:
+            continue
+
+        if dt >= 0:
+            o = open_valves.copy()
+            t_travel = path_len[room[0]][next_valve] + 1
+            o[room[1]] = t_travel >= dt
+            o[next_valve] = t_travel <= dt
+            if t + dt - t_travel > 0:  # update first position
+                J.append(
+                    rate * dt
+                    + maximal_pressure2(
+                        (next_valve, room[1]),
+                        t - dt,
+                        dt - t_travel,
+                        rooms,
+                        good_valves,
+                        path_len,
+                        o,
+                    )
+                )
+        if dt <= 0:
+            o = open_valves.copy()
+            t_travel = path_len[room[1]][next_valve] + 1
+            o[room[0]] = t_travel >= -dt
+            o[next_valve] = t_travel <= -dt
+            if t - dt - t_travel > 0:  # update 2nd position
+                J.append(
+                    rate * -dt
+                    + maximal_pressure2(
+                        (room[0], next_valve),
+                        t + dt,
+                        dt + t_travel,
+                        rooms,
+                        good_valves,
+                        path_len,
+                        o,
+                    )
+                )
+    p = rate * abs(dt)
+    open_valves[room[0]] = True
+    open_valves[room[1]] = True
+    rate = sum([rooms[r][0] for r in good_valves if open_valves[r]])
+    J.append(rate * (t-abs(dt)) + p)
     return max(J)
 
 
@@ -63,9 +119,19 @@ def main():
     }
     open_valves = {name: False for name in good_valves}
     open_valves["AA"] = True
-    print(maximal_pressure("AA", 30, rooms, good_valves, path_len, open_valves)) #1845 right
+    print(
+        maximal_pressure("AA", 30, rooms, good_valves, path_len, open_valves)
+    )  # 1845 right
     t1 = time.time()
     print(f"Time Part 1: {t1-t0:.3}s")
+    print(
+        maximal_pressure2(
+            ("AA", "AA"), 26, 0, rooms, good_valves, path_len, open_valves
+        )
+    )  #2286 right, but takes an eternity, lol
+    t2 = time.time()
+    print(f"Time Part 1: {t2-t1:.1}s")
+
 
 
 if __name__ == "__main__":
