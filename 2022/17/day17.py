b177917fac413b0ef0f52163cbf61a255fb2e0bd
Adrian <haadrian@student.ethz.ch>
2022-12-19 14:55:01 +0100

    22 day17 part 1

--- 2022/17/day17.py
+++ 2022/17/day17.py
@@ -24,20 +24,61 @@ def main():
     #   ##
     rocks.append(([1, 1, 0, 0], [0, 1, 0, 1], 2, 2))
 
-    tetris = [[1, 1, 1, 1, 1, 1, 1]]
+    tetris = [
+        [1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ]
     flow_idx = 0
     rock_idx = 0
+    top_idx = 4 # == len(tetris)
     for _ in range(2022):
-        rock_center = [0, 2]
+        rock_center = [top_idx, 2]
         while True:  # falling
             move = 1 if flow[flow_idx] == ">" else -1
             flow_idx = (flow_idx + 1) % len(flow)
-            #move l/r
-            if 0 <= rock_center[0] + move < 7:
-                rock_center[0] += move
+            # move l/r
+            if 0 <= rock_center[1] + move and rock_center[1] + rocks[rock_idx][3] + move <= 7:
+                # print(f"move {'right' if move == 1 else 'left'}")
+                free = True
+                for dx, dy in zip(rocks[rock_idx][0], rocks[rock_idx][1]):
+                    if rock_center[0] + dx >= top_idx:
+                        continue
+                    x = rock_center[0] + dx
+                    y = rock_center[1] + dy + move
+                    if tetris[x][y]:
+                        free = False
+                        break
+                if free:
+                    rock_center[1] += move
             # check downwards
-            
+            free = True
+            for dx, dy in zip(rocks[rock_idx][0], rocks[rock_idx][1]):
+                if rock_center[0] + dx -1 >= top_idx:
+                    continue
+                x = rock_center[0] + dx - 1
+                y = rock_center[1] + dy
+                if tetris[x][y]:
+                    free = False
+                    break
+            if not free:
+                for _ in range(4 - top_idx + (rock_center[0] + rocks[rock_idx][2] - 1)):
+                    tetris.append([0, 0, 0, 0, 0, 0, 0])
+                    top_idx += 1
+                for dx, dy in zip(rocks[rock_idx][0], rocks[rock_idx][1]):
+                    tetris[rock_center[0]+dx][rock_center[1]+dy] = 1
+                break
+            rock_center[0] -= 1
+        # print_map(tetris)
         rock_idx = (rock_idx + 1) % 5
+    print(top_idx-4) # part1: 3100
+
+
+def print_map(tetris):
+    for r in reversed(tetris):
+        [print("#" if px else " ", end='') for px in r]
+        print("")
 
 
 if __name__ == "__main__":
