edf07f8f518cfc2843a6da8f4f3a62c7d3c29e12
Adrian <haadrian@student.ethz.ch>
2022-12-22 10:47:05 +0100

    Finish 22 day 21

--- 2022/21/day21.py
+++ 2022/21/day21.py
@@ -20,8 +20,22 @@ def main():
         monkeys = {
             s.split(": ")[0]: s.split(": ")[1] for s in f.read().strip().split("\n")
         }
-        print(monkeys)
-    print(call_monkey(monkeys, "root"))  # 81075092088442
+        # print(monkeys)
+    print(f"Part 1: {int(call_monkey(monkeys, 'root'))}")  # 81075092088442
+
+    n = 1
+    for i in range(100):
+        monkeys["humn"] = f"{n}"
+        a1 = call_monkey(monkeys, monkeys["root"][:4])
+        b1 = call_monkey(monkeys, monkeys["root"][-4:])
+        monkeys["humn"] = f"{n+100}"
+        a2 = call_monkey(monkeys, monkeys["root"][:4])
+        b2 = call_monkey(monkeys, monkeys["root"][-4:])
+
+        n = int(n - (a1 - b1) / ((a2 - b2) - (a1 - b1)) * 100)  # Newton step
+        if a1 == b1:
+            print(f"Part 2: {n} | found in {i} Newton steps")  # 3349136384441
+            break
 
 
 if __name__ == "__main__":
