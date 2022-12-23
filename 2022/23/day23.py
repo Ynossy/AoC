31ca98eb50ce5afe3b141f54ffdddf0a9c2ead63
Adrian <haadrian@student.ethz.ch>
2022-12-23 14:47:03 +0100

    22 day 23

--- 2022/23/day23.py
+++ 2022/23/day23.py
@@ -1,39 +1,35 @@
-def print_cave(cave):
-    for s in reversed(cave):
-        for c in s:
-            if c:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print("")
+nswe = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (1, -1),
+    (0, -1),
+    (-1, -1),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+]
 
 
-# adjacent = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
-
-nswe = [(-1, -1), (-1, 0), (-1, 1), 
-        (1, 1), (1, 0), (1, -1),
-        (1, -1), (0, -1), (-1,-1),
-        (-1, 1), (0, 1), (1, 1)]
-
-
-def main():
-    with open("input.txt") as f:
-        cave = [[1 if c == "#" else 0 for c in s] for s in f.read().strip().split("\n")]
-    elves = set(
-        [(i, j) for i in range(len(cave)) for j in range(len(cave[0])) if cave[i][j]]
-    )
+def simulate_elves(elves, part1=True):
     current_pos = elves.copy()
-    for i in range(10):
-        # print(current_pos)
+    it = 10 if part1 else 10000
+    for i in range(it):
+        print(f"Iteration: {i}      ", end="\r")
         next_pos = {}
         next_pos_set = set()
         duplicates_set = set()
+        moved = False
         for elve in current_pos:
             if not any(
                 [(elve[0] + dp[0], elve[1] + dp[1]) in current_pos for dp in nswe]
             ):
                 next_pos[elve] = elve
                 continue
+            moved = True
             for j in range(4):
                 if not any(
                     [
@@ -56,12 +52,31 @@ def main():
                 duplicates_set.add(next_pos[elve])
             else:
                 next_pos_set.add(next_pos[elve])
-        current_pos = set([next_pos[elve] if next_pos[elve] not in duplicates_set else elve for elve in current_pos])    
-        # print(next_pos)
-    x_diff = max([p[0] for p in current_pos])- min([p[0] for p in current_pos])+1
-    y_diff = max([p[1] for p in current_pos])- min([p[1] for p in current_pos])+1
-    print(x_diff,y_diff, x_diff*y_diff-len(current_pos)) # Part 1: 4049
+        current_pos = set(
+            [
+                next_pos[elve] if next_pos[elve] not in duplicates_set else elve
+                for elve in current_pos
+            ]
+        )
+        if not moved:
+            print(f"Part 2: Converged at idx {i+1}")
+            break
+    print("")
+    x_diff = max([p[0] for p in current_pos]) - min([p[0] for p in current_pos]) + 1
+    y_diff = max([p[1] for p in current_pos]) - min([p[1] for p in current_pos]) + 1
+    if part1:
+        print(f"Part1: {x_diff*y_diff-len(current_pos)}")
 
+
+def main():
+    with open("input.txt") as f:
+        cave = [[1 if c == "#" else 0 for c in s] for s in f.read().strip().split("\n")]
+    elves = set(
+        [(i, j) for i in range(len(cave)) for j in range(len(cave[0])) if cave[i][j]]
+    )
+
+    simulate_elves(elves, part1=True)  # Part 1: 4049
+    simulate_elves(elves, part1=False)  # Part2 : 1021
 
 
 if __name__ == "__main__":
