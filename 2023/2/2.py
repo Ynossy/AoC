0a1c326936c7051cac957abcfdabebbc602f0806
Adrian <haadrian@student.ethz.ch>
2023-12-15 12:21:46 +0100

    day2 part 2

--- 2023/2/2.py
+++ 2023/2/2.py
@@ -1,27 +1,35 @@
 import numpy as np
 
+# order [red green blue]
+def parse_max_cubes(game: str) -> dict:
+    max_cubes = {"red": 0, "green": 0, "blue": 0}
+    for _try in game.split(": ")[1].split("; "):
+        for cube in _try.split(", "):
+            n, color = cube.split(" ")
+            max_cubes[color] = max(max_cubes[color], int(n))
+    return max_cubes
+
+
 def part1(data: list[str]):
-    bag = {"red": 12, "green":13, "blue":14}
+    bag = {"red": 12, "green": 13, "blue": 14}
     result = 0
     for id, game in enumerate(data):
-        tries = game.split(":")[1].split(";")
-        possible = True
-        for t in tries:
-            cubes = t.strip().split(", ")
-            for cube in cubes:
-                if int(cube.split(" ")[0]) > bag[cube.split(" ")[1]]:
-                    possible = False
-        if possible:
+        max_cubes = parse_max_cubes(game)
+        if all(i <= j for i, j in zip(max_cubes.items(), bag.items())):
             result += id + 1
     return result
 
 
 def part2(data: list[str]):
-    return 0
+    result = 0
+    for game in data:
+        max_cubes = parse_max_cubes(game)
+        result += max_cubes["red"] * max_cubes["blue"] * max_cubes["green"]
+    return result
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         data = f.read().strip().split("\n")
-    print(f"Part1: {part1(data)}")  # 0
-    print(f"Part2: {part2(data)}")  # 0
+    print(f"Part1: {part1(data)}")  # 2278
+    print(f"Part2: {part2(data)}")  # 67953
