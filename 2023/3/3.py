4be8f292c42a9a7b6fe29d68e570fa906ee39001
Adrian <haadrian@student.ethz.ch>
2023-12-15 13:48:33 +0100

    day3 part 1

---
+++ 2023/3/3.py
@@ -0,0 +1,65 @@
+#%%
+import numpy as np
+
+
+def has_symbol(data, y, start, end):
+    start_idx = start if start == 0 else start - 1
+    end_idx = end if end == len(data[y]) - 1 else end + 1
+
+    if (
+        y != 0
+        and data[y - 1][start_idx : end_idx + 1].count(".") != end_idx + 1 - start_idx
+    ):
+        return True
+
+    if (
+        y != len(data) - 1
+        and data[y + 1][start_idx : end_idx + 1].count(".") != end_idx + 1 - start_idx
+    ):
+        return True
+
+    if start != 0 and data[y][start - 1] != ".":
+        return True
+
+    if end != len(data[y]) - 1 and data[y][end + 1] != ".":
+        return True
+    return False
+
+
+def part1(data: list[str]):
+    result = 0
+    max_y = len(data)
+    max_x = len(data[0])
+    for y in range(max_y):
+        start_idx = 0
+        line = data[y]
+        for x in range(max_x):
+            if line[x].isnumeric() and (x == 0 or not line[x - 1].isnumeric()):
+                start_idx = x
+            if line[x].isnumeric() and (
+                x + 1 == max_x or not line[x + 1].isnumeric()
+            ):
+                if(int(line[start_idx : x + 1])==968):
+                    abc=0
+                if has_symbol(data, y, start_idx, x):
+                    result += int(line[start_idx : x + 1])
+                    print(f"\033[31m{line[start_idx : x + 1]}\033[0m", end="")
+                else:
+                    print(f"\033[32m{line[start_idx : x + 1]}\033[0m", end="")
+            elif(not line[x].isnumeric()):
+                print(line[x], end="")
+        print("")
+    return result
+
+
+def part2(data: list[str]):
+    return 0
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        data = f.read().strip().split("\n")
+    print(f"Part1: {part1(data)}")  # 553769 too low, 553825 right
+    print(f"Part2: {part2(data)}")  # 0
+
+# %%
