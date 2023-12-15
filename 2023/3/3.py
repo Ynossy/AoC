274bbdd1707a4cb3056dd07f758a362fde4a1f99
Adrian <haadrian@student.ethz.ch>
2023-12-15 15:06:14 +0100

    day3 part 2

--- 2023/3/3.py
+++ 2023/3/3.py
@@ -36,11 +36,9 @@ def part1(data: list[str]):
         for x in range(max_x):
             if line[x].isnumeric() and (x == 0 or not line[x - 1].isnumeric()):
                 start_idx = x
-            if line[x].isnumeric() and (
-                x + 1 == max_x or not line[x + 1].isnumeric()
-            ):
-                if(int(line[start_idx : x + 1])==968):
-                    abc=0
+            if line[x].isnumeric() and (x + 1 == max_x or not line[x + 1].isnumeric()):
+                if int(line[start_idx : x + 1]) == 968:
+                    abc = 0
                 if has_symbol(data, y, start_idx, x):
                     result += int(line[start_idx : x + 1])
             #         print(f"\033[31m{line[start_idx : x + 1]}\033[0m", end="")
@@ -51,34 +49,36 @@ def part1(data: list[str]):
         # print("")
     return result
 
-def get_gear_ratio(data, y, x):
-    numbers = []
-    # left 
-    if(x!= 0 and data[y][x-1].isnumeric()):
-        start_idx = x-1
-        while start_idx>=0 and data[y][start_idx].isnumeric(): start_idx-=1
-        numbers.append(int(data[y][start_idx+1:x]))
-    
-    # right
-    if(x!= len(data[y])-1 and data[y][x+1].isnumeric()):
-        end_idx = x+1
-        while end_idx < len(data[y])-1 and data[y][end_idx].isnumeric(): end_idx+=1
-        numbers.append(int(data[y][x+1:end_idx]))
-
-    # top
-    # if(y!=0 and )
-    # WIP
-
-
 
 def part2(data: list[str]):
     result = 0
     max_y = len(data)
     max_x = len(data[0])
+    numbers = [[] for _ in range(max_y)]  # [value, xstart, xend] idx-inclusive
+    stars = []  # [y, x]
     for y in range(max_y):
+        start_idx = 0
+        line = data[y]
         for x in range(max_x):
-            if(data[y][x]=='*'):
-                result += get_gear_ratio(data, y, x)
+            if line[x].isnumeric() and (x == 0 or not line[x - 1].isnumeric()):
+                start_idx = x
+            if line[x].isnumeric() and (x + 1 == max_x or not line[x + 1].isnumeric()):
+                numbers[y].append([int(line[start_idx : x + 1]), start_idx, x])
+            if line[x] == "*":
+                stars.append([y, x])
+
+    for star in stars:
+        y, x = star
+        adjacent = []
+        for dy in range(y - (y > 0), y + (y < len(data)) + 1):
+            for n in numbers[dy]:
+                value, xstart, xend = n
+                if x - (x > 0) <= xstart <= x + (x < len(data[dy])) or x - (
+                    x > 0
+                ) <= xend <= x + (x < len(data[dy])):
+                    adjacent.append(value)
+        if len(adjacent) == 2:
+            result += adjacent[0] * adjacent[1]
     return result
 
 
@@ -86,6 +86,6 @@ if __name__ == "__main__":
     with open("input.txt") as f:
         data = f.read().strip().split("\n")
     print(f"Part1: {part1(data)}")  # 553769 too low, 553825 right
-    print(f"Part2: {part2(data)}")  # 0
+    print(f"Part2: {part2(data)}")  # 93994191
 
 # %%
