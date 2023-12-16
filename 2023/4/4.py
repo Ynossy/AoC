16841e67b8eb843c76969af7b50cbf97c1e144b4
Adrian <haadrian@student.ethz.ch>
2023-12-16 13:40:14 +0100

    day 4 part 2

--- 2023/4/4.py
+++ 2023/4/4.py
@@ -5,19 +5,23 @@ def intersect_card(card):
     n1, n2 = card.split(": ")[1].split(" | ")
     set_n1 = {int(c) for c in n1.split(" ")}
     set_n2 = {int(c) for c in n2.split(" ")}
-    winners = len(set_n1.intersection(set_n2))
-    return 0 if winners == 0 else 2**(winners-1)
+    return len(set_n1.intersection(set_n2))
 
 def part1(data: list[str]):
-    return sum(intersect_card(card) for card in data)
+    winners = [intersect_card(card) for card in data]
+    return sum(0 if n == 0 else 2**(n-1) for n in winners)
 
 
 def part2(data: list[str]):
-    return 0
+    winners = [intersect_card(card) for card in data]
+    card_pile = [1 for _ in range(len(winners))]
+    for i in range(len(winners)):
+        card_pile[i+1:i+1+winners[i]] = [n+card_pile[i] for n in card_pile[i+1:i+1+winners[i]]]
+    return sum(card_pile)
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         data = f.read().strip().split("\n")
-    print(f"Part1: {part1(data)}")  # 0
-    print(f"Part2: {part2(data)}")  # 0
+    print(f"Part1: {part1(data)}")  # 21558
+    print(f"Part2: {part2(data)}")  # 10425665
