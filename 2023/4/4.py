84a5e0465a63f6f7304cccbd01be5e9984d1ed18
Adrian <haadrian@student.ethz.ch>
2023-12-16 13:43:24 +0100

    format

--- 2023/4/4.py
+++ 2023/4/4.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def intersect_card(card):
     card = card.replace("  ", " ")
     n1, n2 = card.split(": ")[1].split(" | ")
@@ -7,16 +8,19 @@ def intersect_card(card):
     set_n2 = {int(c) for c in n2.split(" ")}
     return len(set_n1.intersection(set_n2))
 
+
 def part1(data: list[str]):
     winners = [intersect_card(card) for card in data]
-    return sum(0 if n == 0 else 2**(n-1) for n in winners)
+    return sum(0 if n == 0 else 2 ** (n - 1) for n in winners)
 
 
 def part2(data: list[str]):
     winners = [intersect_card(card) for card in data]
     card_pile = [1 for _ in range(len(winners))]
     for i in range(len(winners)):
-        card_pile[i+1:i+1+winners[i]] = [n+card_pile[i] for n in card_pile[i+1:i+1+winners[i]]]
+        card_pile[i + 1 : i + 1 + winners[i]] = [
+            n + card_pile[i] for n in card_pile[i + 1 : i + 1 + winners[i]]
+        ]
     return sum(card_pile)
 
 
