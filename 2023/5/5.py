292d5403a08345069240e55c45c9501d9d656ebf
Adrian <haadrian@student.ethz.ch>
2023-12-17 17:41:12 +0100

    wip day 5

--- 2023/5/5.py
+++ 2023/5/5.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 
 def convert(idx, mapping):
@@ -19,12 +20,58 @@ def part1(seeds, maps):
     return min(locations)
 
 
+def convert_range(idx_range, mapping):
+    mapping.sort(key=lambda x: x[0])
+    mapped_ranges = []
+    for r_map in mapping:
+        if idx_range[0] <= r_map[1] <= idx_range[0] + idx_range[1]:
+            # mapping starts in range
+            # first part outside map
+            mapped_ranges.append([idx_range[0], idx_range[0] - r_map[1]])
+            # second part inside map
+            if idx_range[0] <= r_map[1] + r_map[2] <= idx_range[0] + idx_range[1]:
+                # ends in range aswell --> done
+                mapped_ranges.append([])
+            # mapped_ranges.append([r_map[0], idx_range[0]- r_map[1]+r_map[2]])
+
+        elif idx_range[0] <= r_map[1] + r_map[2] <= idx_range[0] + idx_range[1]:
+            # mapping ends in range (but doesnt start inside) --> split
+            mapped_ranges.append([r_map[0], idx_range[0] + idx_range[1] - r_map[1]])
+            new_start = r_map[1] + r_map[2] + 1
+            # idx_range = [new_start, idx_range - ]
+        elif (
+            r_map[1] <= idx_range[0]
+            and idx_range[0] + idx_range[1] <= r_map[1] + r_map[2]
+        ):
+            # mapping contains full range --> done
+            mapped_ranges.append([r_map[0] + idx_range[0] - r_map[1], idx_range[1]])
+            return mapped_ranges
+
+    mapped_ranges.append(idx_range)
+    return mapped_ranges
+
+
+def get_location_ranges(ranges, maps):
+    for mapping in maps:
+        mapped_ranges = []
+        for r in ranges:
+            mapped_ranges += convert_range(r, mapping)
+        ranges = mapped_ranges
+        print(ranges)
+        # print(len(ranges))
+    return ranges
+
+
 def part2(seeds, maps):
-    return 0
+    ranges = [[seeds[i], seeds[i + 1]] for i in range(0, len(seeds), 2)]
+
+    ranges = get_location_ranges(ranges, maps)
+    print(ranges)
+    return min(x[0] for x in ranges)
 
 
 if __name__ == "__main__":
-    with open("input.txt") as f:
+    with open("example.txt") as f:
         data = f.read().strip().split("\n")
 
     seeds = [int(i) for i in data[0].split(": ")[1].split(" ")]
