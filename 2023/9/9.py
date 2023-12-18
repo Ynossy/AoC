de88c3a219007d2dfb5b506d5606c94718ebd001
Adrian <haadrian@student.ethz.ch>
2023-12-18 17:41:40 +0100

    cleanup day 9

--- 2023/9/9.py
+++ 2023/9/9.py
@@ -1,38 +1,30 @@
 import numpy as np
 
 
-def part1(data: list[str]):
-    result = 0
-    for line in data:
-        numbers = [[int(i) for i in line.split(" ")]]
-        while any(numbers[-1]):
-            numbers.append(
-                [
-                    numbers[-1][i + 1] - numbers[-1][i]
-                    for i in range(len(numbers[-1]) - 1)
-                ]
-            )
-        for i in reversed(range(len(numbers) - 1)):
+def differentiate_sequences(numbers: list[list[int]]):
+    while any(numbers[-1]):
+        numbers.append(
+            [numbers[-1][i + 1] - numbers[-1][i] for i in range(len(numbers[-1]) - 1)]
+        )
+    return numbers
+
+
+def extrapolate(report, part2=False):
+    numbers = differentiate_sequences([[int(i) for i in report.split(" ")]])
+    for i in reversed(range(len(numbers) - 1)):
+        if part2:
+            numbers[i].insert(0, numbers[i][0] - numbers[i + 1][0])
+        else:
             numbers[i].append(numbers[i][-1] + numbers[i + 1][-1])
-        result += numbers[0][-1]
-    return result
+    return numbers[0][0 if part2 else -1]
+
+
+def part1(data: list[str]):
+    return sum(extrapolate(report) for report in data)
 
 
 def part2(data: list[str]):
-    result = 0
-    for line in data:
-        numbers = [[int(i) for i in line.split(" ")]]
-        while any(numbers[-1]):
-            numbers.append(
-                [
-                    numbers[-1][i + 1] - numbers[-1][i]
-                    for i in range(len(numbers[-1]) - 1)
-                ]
-            )
-        for i in reversed(range(len(numbers) - 1)):
-            numbers[i].insert(0, numbers[i][0] - numbers[i + 1][0])
-        result += numbers[0][0]
-    return result
+    return sum(extrapolate(report, True) for report in data)
 
 
 if __name__ == "__main__":
