de7203e8c236170c0da76135d5d40aa941e31fc4
Adrian <haadrian@student.ethz.ch>
2023-12-19 17:23:58 +0100

    cleanup day 12

--- 2023/12/12.py
+++ 2023/12/12.py
@@ -1,25 +1,26 @@
-import numpy as np
 from tqdm import tqdm
 
+lut = None
 
-def place_springs(m: str, groups: list[int]):
+
+def place_springs(m: str, groups: list[int], cache=False):
     global lut
-    if lut[-len(groups)][-len(m)] is not None:
-        return lut[-len(groups)][-len(m)]
+    if cache: # initialize cache
+        lut = [[None for _ in range(len(m))] for _ in range(len(groups))]
+    if lut and lut[-len(groups)][-len(m)] is not None:
+        return lut[-len(groups)][-len(m)]  # infinite speedup
     if not groups:
         return "#" not in m
     if len(m) < sum(groups) + len(groups) - 1:
-        return 0
+        return 0  # 50% speedup
     combinations = 0
     g_len = groups[0]
     first_spring = m.find("#")
-    qs = [
+    qs = [  # all '?' up to (and including) the first '#' are candidates for placement
         i
         for i, c in enumerate(m)
-        if c == "?" and (first_spring == -1 or i < first_spring)
+        if c in ["?", "#"] and (first_spring == -1 or i <= first_spring)
     ]
-    if first_spring != -1:
-        qs.append(first_spring)
     for q in qs:
         if q + g_len > len(m):
             break
@@ -27,7 +28,8 @@ def place_springs(m: str, groups: list[int]):
             q + g_len == len(m) or m[q + g_len] != "#"
         ):
             combinations += place_springs(m[q + g_len + 1 :], groups[1:])
-    lut[-len(groups)][-len(m)] = combinations
+    if lut:
+        lut[-len(groups)][-len(m)] = combinations
     return combinations
 
 
@@ -36,14 +38,7 @@ def part1(data: list[list[str, list[int]]]):
 
 
 def part2(data: list[list[str, list[int]]]):
-    global lut
-    combinations = 0
-    for m, group in tqdm(data):
-        m += f"?{m}" * 4
-        group *= 5
-        lut = [[None for _ in range(len(m))] for _ in range(len(group))]
-        combinations += place_springs(m, group)
-    return combinations
+    return sum(place_springs(m + f"?{m}" * 4, group * 5, True) for m, group in data)
 
 
 if __name__ == "__main__":
@@ -52,5 +47,5 @@ if __name__ == "__main__":
             [l.split(" ")[0], [int(n) for n in l.split(" ")[1].split(",")]]
             for l in f.read().strip().split("\n")
         ]
-    # print(f"Part1: {part1(data)}")  # 6871
+    print(f"Part1: {part1(data)}")  # 6871
     print(f"Part2: {part2(data)}")  # 2043098029844
