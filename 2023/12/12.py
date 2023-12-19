032a3bc7beb118f9b64785e68cbd8bd92816219f
Adrian <haadrian@student.ethz.ch>
2023-12-19 16:17:14 +0100

    day 12 part 1

--- 2023/12/12.py
+++ 2023/12/12.py
@@ -6,12 +6,9 @@ def place_springs(m: str, groups: list[int]):
     combinations = 0
     g_len = groups[0]
     first_spring = m.find("#")
-    qs = [i for i,c in enumerate(m) if c=='?' and i < first_spring]
+    qs = [i for i,c in enumerate(m) if c=='?' and (first_spring == -1 or i < first_spring)]
     if(first_spring != -1): 
         qs.append(first_spring)
-    assert qs
-    if first_spring <= qs[0] and m[first_spring:first_spring+g_len].find('.') == -1:
-        return 1
     for q in qs:
         if(q+g_len > len(m)):
             break
@@ -24,8 +21,9 @@ def place_springs(m: str, groups: list[int]):
 def part1(data: list[list[str]]):
     combinations = 0
     for m, group in data:
-        combinations += place_springs(m, group)
-        print(combinations)
+        c = place_springs(m, group)
+        print(c)
+        combinations += c
     return combinations
 
 
@@ -34,10 +32,10 @@ def part2(data: list[list[str]]):
 
 
 if __name__ == "__main__":
-    with open("12/example.txt") as f:
+    with open("input.txt") as f:
         data = [
             [l.split(" ")[0], [int(n) for n in l.split(" ")[1].split(",")]]
             for l in f.read().strip().split("\n")
         ]
-    print(f"Part1: {part1(data)}")  # 0
+    print(f"Part1: {part1(data)}")  # 6871
     print(f"Part2: {part2(data)}")  # 0
