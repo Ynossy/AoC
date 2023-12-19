a6ab1ab6cdfaec1f380852c421d17d3d64495dab
Adrian <haadrian@student.ethz.ch>
2023-12-19 18:54:35 +0100

    day 13 part 2

--- 2023/13/13.py
+++ 2023/13/13.py
@@ -1,46 +1,36 @@
 import numpy as np
 
 
-def get_v_mirror(pattern: list[str]):
+def get_v_mirror(pattern: list[str], errors=0):
     for i in range(len(pattern[0]) - 1):
-        if all(
-            pattern[j][i] == pattern[j][i + 1] for j in range(len(pattern))
-        ) and is_v_symmetric(pattern, i):
+        if (
+            sum(pattern[j][i] != pattern[j][i + 1] for j in range(len(pattern)))
+            <= errors
+            and v_sym_errors(pattern, i) == errors
+        ):
             return i + 1
     return 0
 
 
-def is_v_symmetric(pattern: list[str], idx):
-    return all(
-        all(pattern[j][idx - i] == pattern[j][idx + i + 1] for j in range(len(pattern)))
+def v_sym_errors(pattern: list[str], idx):
+    return sum(
+        sum(pattern[j][idx - i] != pattern[j][idx + i + 1] for j in range(len(pattern)))
         for i in range(min(idx + 1, len(pattern[0]) - idx - 1))
     )
 
 
-def get_h_mirror(pattern: list[str]):
-    for i in range(len(pattern) - 1):
-        if pattern[i] == pattern[i + 1] and is_h_symmetric(pattern, i):
-            return i + 1
-    return 0
-
-
-def is_h_symmetric(pattern: list[str], idx):
-    return all(
-        pattern[idx - i] == pattern[idx + 1 + i]
-        for i in range(min(idx + 1, len(pattern) - idx - 1))
-    )
-
-
 def part1(data: list[list[str]]):
-    return sum(get_v_mirror(pattern) + 100 * get_h_mirror(pattern) for pattern in data)
+    return sum(get_v_mirror(pattern) + 100 * get_v_mirror(list(zip(*pattern))) for pattern in data)
 
 
 def part2(data: list[list[str]]):
-    return 0
+    return sum(
+        get_v_mirror(pattern, 1) + 100 * get_v_mirror(list(zip(*pattern)), 1) for pattern in data
+    )
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         data = [m.split("\n") for m in f.read().strip().split("\n\n")]
     print(f"Part1: {part1(data)}")  # 30575
-    print(f"Part2: {part2(data)}")  # 0
+    print(f"Part2: {part2(data)}")  # 37478
