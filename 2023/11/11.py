907e58513101596202f5665d92ac69e00f40c6a4
Adrian <haadrian@student.ethz.ch>
2023-12-19 12:35:40 +0100

    day 11 part 2

--- 2023/11/11.py
+++ 2023/11/11.py
@@ -2,25 +2,15 @@ import numpy as np
 from scipy.spatial.distance import cdist
 
 
-def part1(data: np.ndarray):
-    data = np.insert(
-        data,
-        np.argwhere(data.sum(axis=1) == 0).flatten(),
-        np.zeros((data.shape[1],)),
-        axis=0,
-    )
-    data = np.insert(
-        data,
-        np.argwhere(data.sum(axis=0) == 0).flatten(),
-        np.zeros((data.shape[0], 1)),
-        axis=1,
-    )
+def expand(data: np.ndarray, n: int):
     stars = np.vstack(np.where(data)).T
-    return int(cdist(stars, stars, metric="cityblock").sum()/2)
-
-
-def part2(data: np.ndarray):
-    return 0
+    expansion = np.zeros(stars.shape)
+    for r in np.argwhere(data.sum(axis=1) == 0).flatten():
+        expansion[stars[:, 0] > r, 0] += n
+    for c in np.argwhere(data.sum(axis=0) == 0).flatten():
+        expansion[stars[:, 1] > c, 1] += n
+    stars = stars + expansion
+    return int((cdist(stars, stars, metric="cityblock") / 2).sum())
 
 
 if __name__ == "__main__":
@@ -28,5 +18,5 @@ if __name__ == "__main__":
         data = np.array(
             [[int(c == "#") for c in s] for s in f.read().strip().split("\n")]
         )
-    print(f"Part1: {part1(data)}")  # 9418609
-    print(f"Part2: {part2(data)}")  # 0
+    print(f"Part1: {expand(data, 1)}")  # 9418609
+    print(f"Part2: {expand(data, 999999)}")  # 593821230983
