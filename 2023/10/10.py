c4bf01d6bd6ccfc03140129009ea817386416fd4
Adrian <haadrian@student.ethz.ch>
2023-12-19 11:46:38 +0100

    day 10 part 2

--- 2023/10/10.py
+++ 2023/10/10.py
@@ -22,30 +22,50 @@ def pad_map(data: list[list[str]]):
     data.append(["." for _ in range(len(data[0]))])
 
 
-def part1(data: list[str]):
+def part1(data: list[list[str]], path_map: list[list[str]]):
     pad_map(data)
+    pad_map(path_map)
     idx = [[i, line.index("S")] for i, line in enumerate(data) if "S" in line][0]
     direction = [
         x
         for x in dirs.keys()
         if x in pipes[data[idx[0] + dirs[x][0]][idx[1] + dirs[x][1]]].keys()
     ][0]
+    starting_dir = direction
     steps = 0
     while True:
+        path_map[idx[0]][idx[1]] = data[idx[0]][idx[1]]
         idx[0], idx[1] = idx[0] + dirs[direction][0], idx[1] + dirs[direction][1]
         if data[idx[0]][idx[1]] == "S":
             break
         steps += 1
         direction = pipes[data[idx[0]][idx[1]]][direction]
+
+    # patch start field to pipe for part 2
+    starting_dir = {"N": "S", "S": "N", "W": "E", "E": "W"}[starting_dir]
+    for pipe in pipes.keys():
+        if starting_dir in pipes[pipe] and direction in pipes[pipe]:
+            path_map[idx[0]][idx[1]] = pipe
     return int((steps + 1) / 2)
 
 
-def part2(data: list[str]):
-    return 0
+def part2(path_map: list[str]):
+    area = 0
+    for d in path_map:
+        inside = False
+        last_cut = None
+        for c in d:
+            if c in ["|", "L", "F"] or (last_cut == 'L' and c == 'J') or (last_cut == 'F' and c == '7'):
+                inside = not inside
+                last_cut = c
+            if inside and c == ".":
+                area += 1
+    return area
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         data = [list(s) for s in f.read().strip().split("\n")]
-    print(f"Part1: {part1(data)}")  # 6649
-    print(f"Part2: {part2(data)}")  # 0
+        path_map = [["." for _ in range(len(l))] for l in data]
+    print(f"Part1: {part1(data, path_map)}")  # 6649
+    print(f"Part2: {part2(path_map)}")  # 601
