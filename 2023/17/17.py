c1c90afd4272a4c5a90c8aab34902aafadb28eff
Adrian <adrian.hartmann@bluewin.ch>
2023-12-28 17:41:57 +0100

    Fix day 17 part 2

--- 2023/17/17.py
+++ 2023/17/17.py
@@ -8,10 +8,12 @@ def find_path(data, n_fw_min, n_fw_max):
     min_heap = []
     heapq.heappush(min_heap, (0, 0, 0, 2, -1))  # [heatloss, y, x, heading, distance]
     distances = [
-        [[float("inf")] * 2 * n_fw_max for _ in range(len(data[0]))]
+        [[float("inf")] * 4 * n_fw_max for _ in range(len(data[0]))]
         for _ in range(len(data))
     ]
-    distances[0][0] = [0] * 2 * n_fw_max  # every stepcount v and h
+    distances[0][0] = (
+        [0] * 4 * n_fw_max
+    )  # every stepcount in every direction in v and h
     while (
         min_heap[0][1] != len(data) - 1
         or min_heap[0][2] != len(data[0]) - 1
@@ -19,13 +21,11 @@ def find_path(data, n_fw_min, n_fw_max):
     ):
         loss, y, x, heading, d = heapq.heappop(min_heap)
         for ddir in [-1, 0, 1]:
-            if ddir == 0 and d >= n_fw_max - 1:
-                continue
-            elif ddir != 0 and 0 <= d < n_fw_min - 1:
+            if ddir == 0 and d >= n_fw_max - 1 or ddir != 0 and 0 <= d < n_fw_min - 1:
                 continue
             new_dir = (heading + ddir) % 4
             new_d = d + 1 if ddir == 0 else 0
-            state_idx = (new_dir % 2) * n_fw_max + new_d
+            state_idx = new_dir * n_fw_max + new_d
             dy, dx = directions[new_dir]
             if (
                 0 <= y + dy < len(data)
@@ -56,8 +56,8 @@ def part2(data: list[list[int]]):
 
 if __name__ == "__main__":
     t = time.time()
-    with open("example2.txt") as f:
+    with open("input.txt") as f:
         data = [[int(c) for c in row] for row in f.read().strip().split("\n")]
     print(f"Part1: {part1(data)}")  # 646 too high, 635 right
-    print(f"Part2: {part2(data)}")  # 735 too high, 737 too high, 734 from reddit right?
+    print(f"Part2: {part2(data)}")  # 735 too high, 737 too high, 734 right
     print(f"Runtime: {time.time()-t}")
