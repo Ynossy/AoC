b7c9fc7d271f6a19e7839776ec7f1902afc240a1
Adrian <haadrian@student.ethz.ch>
2024-12-02 19:34:55 +0100

    refactor 24 day2

--- 2024/2/2.py
+++ 2024/2/2.py
@@ -3,16 +3,17 @@ with open("input.txt") as f:
     reports = [[int(y) for y in x.strip().split(" ")] for x in f.readlines()]
 # %%
 
-count = 0
-diffs = [[report[i+1] - report[i] for i in range(len(report)-1)]for report in reports]
-for report in diffs:
-    increasing = all(x>0 for x in report)
-    decreasing = all(x<0 for x in report)
+def check_report(report):
+    diff = [report[i+1] - report[i] for i in range(len(report)-1)]
+    increasing = all(x>0 for x in diff)
+    decreasing = all(x<0 for x in diff)
 
-    levels = all(abs(x) <= 3 for x in report)
-    if (increasing or decreasing) and levels:
-        count += 1
-print(f"Part 1: {count}")
+    levels = all(abs(x) <= 3 for x in diff)
+    return (increasing or decreasing) and levels
+    
+safe_reports = sum(check_report(report) for report in reports)
+
+print(f"Part 1: {safe_reports}")
 # %%
 count = 0
 
@@ -20,42 +21,21 @@ for report in reports:
     diff = [report[i+1] - report[i] for i in range(len(report)-1)]
     increasing = sum(x>0 for x in diff)
     decreasing = sum(x<0 for x in diff)
+    sign = 1 if increasing > decreasing else -1
     safe = True
     for i in range(len(diff)):
-        if increasing > decreasing :
-            if diff[i] <= 0 or diff[i] > 3:
-                safe = False
-                break
-        else:
-            if diff[i] >= 0 or diff[i] < -3:
-                safe = False
-                break
-    if safe:
-        count += 1
-        continue
-    
+        if sign*diff[i] <= 0 or sign*diff[i] > 3:
+            safe = False
+            break
+    # remove first potential issue
     list_v1 = report.copy()
     list_v1.pop(i)
-    diff = [list_v1[i+1] - list_v1[i] for i in range(len(list_v1)-1)]
-    increasing = all(x>0 for x in diff)
-    decreasing = all(x<0 for x in diff)
-
-    levels = all(abs(x) <= 3 for x in diff)
-    if (increasing or decreasing) and levels:
-        count += 1
-        continue
-    
+    # remove second potential issue
     list_v2 = report.copy()
     list_v2.pop(i+1)
-    diff = [list_v2[i+1] - list_v2[i] for i in range(len(list_v2)-1)]
-    increasing = all(x>0 for x in diff)
-    decreasing = all(x<0 for x in diff)
 
-    levels = all(abs(x) <= 3 for x in diff)
-    if (increasing or decreasing) and levels:
+    if safe or check_report(list_v1) or check_report(list_v2):
         count += 1
-        continue
 
 print(f"Part 2: {count}")
-
 # %%
