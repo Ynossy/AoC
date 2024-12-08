c85185e4eecb4a465a643635248459e53b9f3ef9
Adrian <haadrian@student.ethz.ch>
2024-12-08 13:04:10 +0100

    add 24 day 7 part 2

--- 2024/7/7.py
+++ 2024/7/7.py
@@ -7,21 +7,25 @@ with open("input.txt") as f:
         solution, operands = operation.split(": ")
         operations.append([int(solution), [int(x) for x in operands.split(" ")]])
 # %%
-operators = [lambda a,b : a*b, lambda a,b : a+b]
+operators = [lambda a,b : a*b, lambda a,b : a+b, lambda a,b :int(str(a)+str(b))]
 
-def op(_result, _current, _operands, _opIdx):
+def op(_result, _current, _operands, _opIdx, part2 = False):
     if len(_operands) == 1:
         return operators[_opIdx](_current, _operands[0]) == _result
     if _current > _result:
         return False
     _current = operators[_opIdx](_current, _operands[0])
-    if op(_result, _current, _operands[1:], 0):
+    if part2 and op(_result, _current, _operands[1:], 2, part2):
         return True
-    return op(_result, _current, _operands[1:], 1)
+    if op(_result, _current, _operands[1:], 0, part2):
+        return True
+    return op(_result, _current, _operands[1:], 1, part2)
 
 # for operation in operations:
-#     print(f"{op(operation[0], 0, operation[1], 1)}")
+#     print(f"{operation[0]}: {op(operation[0], 0, operation[1], 1, part2=True)}")
 
 res = sum(operation[0] for operation in operations if op(operation[0], 0, operation[1], 1))
 print(f"Part 1: {res}")
+res = sum(operation[0] for operation in operations if op(operation[0], 0, operation[1], 1, part2=True))
+print(f"Part 2: {res}")
 # %%
