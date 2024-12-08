8e52f77f905063117cf9817d999d000c63388a4f
Adrian <haadrian@student.ethz.ch>
2024-12-08 17:21:17 +0100

    add 24 day 8 part 2

--- 2024/8/8.py
+++ 2024/8/8.py
@@ -1,6 +1,5 @@
 # %%
-
-with open("input.txt") as f:
+with open("example.txt") as f:
     data = [x.strip() for x in f.readlines()]
     antennas = {}
     dim_x = len(data)
@@ -9,25 +8,34 @@ with open("input.txt") as f:
         for y in range(dim_y):
             if data[x][y] == ".":
                 continue
-            print(data[x][y])
             if data[x][y] in antennas:
                 antennas[data[x][y]].append((x, y))
             else:
                 antennas[data[x][y]] = [(x, y)]
 
-# %%
-antinodes = set()
-for antenna in antennas:
-    for a in antennas[antenna]:
-        for b in antennas[antenna]:
-            if a == b:
-                continue
-            dx = a[0] - b[0]
-            dy = a[1] - b[1]
-            a_x = a[0] + dx
-            a_y = a[1] + dy
-            if 0 <= a_x < dim_x and 0 <= a_y < dim_y:
-                antinodes.add((a_x, a_y))
-print(f"Part 1: {len(antinodes)}")
 
+# %%
+def countAntinodes(part2=False):
+    antinodes = set()
+    for antenna in antennas:
+        for a in antennas[antenna]:
+            for b in antennas[antenna]:
+                if a == b:
+                    continue
+                dx = a[0] - b[0]
+                dy = a[1] - b[1]
+                a_x = b[0] + dx if part2 else a[0] + dx
+                a_y = b[1] + dy if part2 else a[1] + dy
+                while 0 <= a_x < dim_x and 0 <= a_y < dim_y:
+                    antinodes.add((a_x, a_y))
+                    if not part2:
+                        break
+                    a_x += dx
+                    a_y += dy
+
+    return len(antinodes)
+
+
+print(f"Part 1: {countAntinodes()}")
+print(f"Part 2: {countAntinodes(part2=True)}")
 # %%
