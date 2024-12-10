f3d5fc57af1d48a4d3fb715c3b138a0338cfe244
Adrian <haadrian@student.ethz.ch>
2024-12-10 17:54:56 +0100

    add 24 day 9 part 2

--- 2024/9/9.py
+++ 2024/9/9.py
@@ -47,3 +47,39 @@ print(f"Part 1: {result}")
 # 6'281'129'828'621 too high
 # 6'279'058'075'753
 # %%
+
+
+ISSPACE = 0
+LENGTH = 1
+FILE_ID = 2
+
+file_id_right = len(data)//2
+
+filesystem = [[i%2!=0, x, i//2 if i%2==0 else -1] for i, x in enumerate(data)]
+
+while file_id_right >= 0:
+    file_idx_to_move = None
+    for i in range(len(filesystem)):
+        if filesystem[i][FILE_ID] == file_id_right:
+            file_idx_to_move = i
+            break
+    for i in range(file_idx_to_move):
+        if filesystem[i][ISSPACE] and (filesystem[i][LENGTH] >= filesystem[file_idx_to_move][LENGTH]):
+            filesystem[i][LENGTH] -= filesystem[file_idx_to_move][LENGTH]
+            temp = filesystem[file_idx_to_move].copy()
+            filesystem[file_idx_to_move][ISSPACE] = True
+            filesystem[file_idx_to_move][FILE_ID] = -1
+            filesystem.insert(i, temp)
+            break
+    file_id_right -= 1
+block = 0
+result = 0
+for f in filesystem:
+    for _ in range(f[LENGTH]):
+        if not f[ISSPACE]:
+            result += block*f[FILE_ID]
+        block += 1
+
+print(f"Part 2: {result}") # takes 18 seconds, but works
+
+# %%
