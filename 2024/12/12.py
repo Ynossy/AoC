47763e76e99688e75d3c4d9cfbc3f4ea236c6444
Adrian <haadrian@student.ethz.ch>
2024-12-14 12:05:44 +0100

    add 24 day 12 p2

--- 2024/12/12.py
+++ 2024/12/12.py
@@ -5,8 +5,25 @@ with open("input.txt") as f:
 # %%
 
 dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+"""
+Corners are either 
+##      #O
+#O  or  OO in all 4 directions 
+"""
+cornersDir = [
+    [[0,1],[1,0],[1,1]],
+    [[0,1],[-1,0],[-1,1]],
+    [[0,-1],[1,0],[1,-1]],
+    [[0,-1],[-1,0],[-1,-1]],
+]
+
+def inRange(x,y):
+    return 0 <= x < len(plants) and 0 <= y < len(plants[0])
+
 visited = [[False for _ in row] for row in plants]
-res = 0
+res1 = 0
+res2 = 0
 for i in range(len(plants)):
     for j in range(len(plants[i])):
         if visited[i][j]:
@@ -16,12 +33,13 @@ for i in range(len(plants)):
         visited[i][j] = True
         area = 1
         perimeter = 0
+        corners = 0
         while flood:
             cur = flood.pop()
             for d in dirs:
                 nx = d[0] + cur[0]
                 ny = d[1] + cur[1]
-                if not (0 <= nx < len(plants) and 0 <= ny < len(plants[0])):
+                if not inRange(nx,ny):
                     perimeter += 1
                 elif plants[nx][ny] == plant:
                     if not visited[nx][ny]:
@@ -30,8 +48,25 @@ for i in range(len(plants)):
                         visited[nx][ny] = True
                 else:
                     perimeter += 1
-        # print(f"Plant: {plant}, area: {area}, perimeter {perimeter}")
-        res += area * perimeter
-print(f"Part 1: {res}")
+            for c in cornersDir:
+                n1x = cur[0] + c[0][0]
+                n1y = cur[1] + c[0][1]
+                n2x = cur[0] + c[1][0]
+                n2y = cur[1] + c[1][1]
+                n3x = cur[0] + c[2][0]
+                n3y = cur[1] + c[2][1]
+                n1Same = inRange(n1x, n1y) and plants[n1x][n1y] == plant
+                n2Same = inRange(n2x, n2y) and plants[n2x][n2y] == plant
+                n3Same = inRange(n3x, n3y) and plants[n3x][n3y] == plant
+
+                if not (n1Same or n2Same) or (n1Same and n2Same and not n3Same):
+                    corners += 1
+        # print(f"Plant: {plant}, area: {area}, perimeter {perimeter}, corners: {corners}")
+        res1 += area * perimeter
+        res2 += area * corners
+print(f"Part 1: {res1}, Part2: {res2}")
+
+#p2: 865044 too low
+# 872382 right
 
 # %%
