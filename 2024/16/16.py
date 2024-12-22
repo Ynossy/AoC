5b76c91486f14ab3e5393eae6beacbe4aa80cede
Adrian <haadrian@student.ethz.ch>
2024-12-22 13:36:22 +0100

    add 24 day 16 p2

--- 2024/16/16.py
+++ 2024/16/16.py
@@ -1,5 +1,5 @@
 # %%
-
+from heapq import heapify, heappush, heappop 
 
 def parseChar(c):
     return float("inf") if c in "S.E" else -1
@@ -15,26 +15,51 @@ with open("input.txt") as f:
 
 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 # %%
+nodes = []
+heapify(nodes)
+heappush(nodes, (0 ,([start], 0, 0)))  # (heap score, (path, direction, score))
 
-nodes = [start + (0, 0)]  # (x, y, direction, score)
-
-X, Y, D, S = 0, 1, 2, 3
+PATH, D, S = 0, 1, 2
 
 maze = [row.copy() for row in initial_maze]
-maze[start[X]][start[Y]] = 0
+maze[start[0]][start[1]] = 0
+
+optimalSet = {start, end}
+paths = []
 
 while nodes:
-    node = nodes.pop()
-    # move forward
+    node = heappop(nodes)[1]
+    if node[S] > maze[end[0]][end[1]]:
+        break
     for i in [-1, 0, 1]:
         nd = (node[D] + i) % len(directions)
-        nx = node[X] + directions[nd][X]
-        ny = node[Y] + directions[nd][Y]
+        nx = node[PATH][-1][0] + directions[nd][0]
+        ny = node[PATH][-1][1] + directions[nd][1]
+        nx2 = node[PATH][-1][0] + 2*directions[nd][0]
+        ny2 = node[PATH][-1][1] + 2*directions[nd][1]
         dS = 1001 if i != 0 else 1
-        if maze[nx][ny] != -1 and node[S] + dS < maze[nx][ny]:
-            maze[nx][ny] = node[S] + dS
-            nodes.append((nx, ny, nd, node[S] + dS))
+        if maze[nx][ny] != -1:
+            if node[S] + dS <= maze[nx][ny] and node[S] + dS <= maze[end[0]][end[1]]:
+                maze[nx][ny] = node[S] + dS
+                if nx == end[0] and ny == end[1]:
+                    paths.append((node[S] + dS, node[PATH]))
+                else:
+                    heappush(nodes, (node[S],(node[PATH]+[(nx, ny)], nd, node[S] + dS)))
+            elif (maze[nx2][ny2] != -1 
+                  and node[S] + dS + 1 <= maze[nx2][ny2] and node[S] + dS + 1 <= maze[end[0]][end[1]]):
+                # edge case where we go straight over a node where already a turn occured
+                if nx2 == end[0] and ny2 == end[1]:
+                    paths.append((node[S]+ dS + 1, node[PATH]+[(nx, ny)]))
+                else:
+                    heappush(nodes, (node[S],(node[PATH]+[(nx, ny),(nx2, ny2)], nd, node[S] + dS + 1)))
 
 print(f"Part 1: {maze[1][-2]}")
 
+for p in paths:
+    if p[0] != maze[1][-2]:
+        continue
+    for n in p[1]:
+        optimalSet.add(n)
+
+print(f"Part 2: {len(optimalSet)}")
 # %%
