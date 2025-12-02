05565beea5e54c3e19989d57609485896430b8ce
Adrian <haadrian@student.ethz.ch>
2025-12-02 19:09:46 +0100

    cleanup 25 day 2

--- 2025/2/2.py
+++ 2025/2/2.py
@@ -1,43 +1,35 @@
-
-#%% 
+# %%
 
 with open("input.txt") as f:
     data = [[int(y) for y in x.split("-")] for x in f.readline().strip().split(",")]
 
-#%% part1
+
+# %% part1
 def checkRepetition(num_int: int):
     num = str(num_int)
-    return num[:len(num)//2] == num[len(num)//2:]
+    return num[: len(num) // 2] == num[len(num) // 2 :]
 
-result = 0
-for x,y in data:
-    for i in range(x,y+1):
-        if checkRepetition(i):
-            result += i
-print(f"Part1: {result}") # 54641809925
-    
+
+result = sum(i for x, y in data for i in range(x, y + 1) if checkRepetition(i))
+print(f"Part1: {result}")  # 54641809925
+
+
 # %% part 2
-
 def checkRepetitionMulti(num_int: int):
     num = str(num_int)
-    for i in range(2,len(num)+1):
+    for i in range(2, len(num) + 1):
         if len(num) % i != 0:
             continue
-        l = len(num)//i
-        values = []
-        for x in range(i-1):
-            a = num[l*x:l*(x+1)]
-            b = num[l*(x+1):l*(x+2)]
-            values.append(a==b)
-        if all(values):
+        l = len(num) // i
+        if all(
+            num[l * x : l * (x + 1)] == num[l * (x + 1) : l * (x + 2)]
+            for x in range(i - 1)
+        ):
             return True
     return False
 
-result = 0
-for x,y in data:
-    for i in range(x,y+1):
-        if checkRepetitionMulti(i):
-            result += i
-print(f"Part2: {result}") # 73694270688
+
+result = sum(i for x, y in data for i in range(x, y + 1) if checkRepetitionMulti(i))
+print(f"Part2: {result}")  # 73694270688
 
 # %%
