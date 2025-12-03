44f84d8f892fd806f229a5be1845498424a124c3
Adrian <haadrian@student.ethz.ch>
2025-12-03 17:43:03 +0100

    cleanup 25 day 3

--- 2025/3/3.py
+++ 2025/3/3.py
@@ -1,39 +1,26 @@
 # %%
-
 with open("input.txt") as f:
     data = f.read().strip().split("\n")
+
+
 # %%
-result = 0
-for bank in data:
-    max1 = 0
-    max2 = 0
+def getMaxJoltage(bank, num_batteries):
+    result = 0
+    max_values = [0] * num_batteries
     for i in range(len(bank)):
         joltage = int(bank[i])
-        if joltage > max1 and i < len(bank) - 1:
-            max1 = joltage
-            max2 = 0
-        elif joltage > max2:
-            max2 = joltage
-    result += 10 * max1 + max2
-
-print(f"Part1: {result}")  # 16842
-# %%
-
-result = 0
-for bank in data:
-    max_values = [0]*12
-    for i in range(len(bank)):
-        joltage = int(bank[i])
-        for j in range(12):
-            if joltage > max_values[j] and i < len(bank) - 12 + 1 + j:
+        for j in range(num_batteries):
+            if joltage > max_values[j] and i < len(bank) - num_batteries + 1 + j:
                 max_values[j] = joltage
-                max_values[j+1:] = [0]*(12-1-j)
+                max_values[j + 1 :] = [0] * (num_batteries - 1 - j)
                 break
-        # print(max_values)
     battery_voltage = 0
     for v in max_values:
         battery_voltage = battery_voltage * 10 + v
     result += battery_voltage
+    return result
 
-print(f"Part1: {result}")  # 167523425665348
+
+print(f"Part1: {sum(getMaxJoltage(bank, 2) for bank in data)}")  # 16842
+print(f"Part2: {sum(getMaxJoltage(bank, 12) for bank in data)}")  # 167523425665348
 # %%
