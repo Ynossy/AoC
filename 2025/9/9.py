8fb21bdf6194c4f27104c3deb3abe840755e20d7
Adrian <adrian.hartmann@bluewin.ch>
2025-12-25 15:41:04 +0100

    cleanup 25 day 9

--- 2025/9/9.py
+++ 2025/9/9.py
@@ -2,7 +2,6 @@
 with open("input.txt") as f:
     tiles = [[int(x) for x in line.split(",")] for line in f.read().strip().split("\n")]
 # %%
-
 max_area = 0
 for i in range(len(tiles) - 1):
     for j in range(i + 1, len(tiles)):
@@ -24,8 +23,11 @@ coord_y = sorted(list(coord_y))
 # note: create an emtpy border of size 1 to floodfill the border around the closed shape
 coord_to_grid_x = {coord_x[i]: i + 1 for i in range(len(coord_x))}
 coord_to_grid_y = {coord_y[i]: i + 1 for i in range(len(coord_y))}
-
 grid = [["." for _ in range(len(coord_y) + 2)] for _ in range(len(coord_x) + 2)]
+
+
+def convertTile(tile: tuple):
+    return coord_to_grid_x[tile[1]], coord_to_grid_y[tile[0]]
 
 
 def increment(x1, x2):
@@ -36,11 +38,10 @@ def increment(x1, x2):
     return 0
 
 
+# fill path from tile coordinates
 for i in range(len(tiles)):
-    x1 = coord_to_grid_x[tiles[i - 1][1]]
-    y1 = coord_to_grid_y[tiles[i - 1][0]]
-    x2 = coord_to_grid_x[tiles[i][1]]
-    y2 = coord_to_grid_y[tiles[i][0]]
+    x1, y1 = convertTile(tiles[i - 1])
+    x2, y2 = convertTile(tiles[i])
     grid[x1][y1] = "X"
     while x1 != x2 or y1 != y2:
         x1 += increment(x1, x2)
@@ -60,12 +61,8 @@ while next_cells:
         ):
             next_cells.append((x + dx, y + dy))
 
-# if the full border is included (red/green tiles), the area must valid
-def verifyArea(tile1: tuple, tile2: tuple):
-    x = x1 = coord_to_grid_x[tile1[1]]
-    y = y1 = coord_to_grid_y[tile1[0]]
-    x2 = coord_to_grid_x[tile2[1]]
-    y2 = coord_to_grid_y[tile2[0]]
+
+def checkPath(x, y, x2, y2):
     while x != x2:
         x += increment(x, x2)
         if grid[x][y] == "#":
@@ -74,14 +71,17 @@ def verifyArea(tile1: tuple, tile2: tuple):
         y += increment(y, y2)
         if grid[x][y] == "#":
             return False
-    while x != x1:
-        x += increment(x, x1)
-        if grid[x][y] == "#":
-            return False
-    while y != y1:
-        y += increment(y, y1)
-        if grid[x][y] == "#":
-            return False
+    return True
+
+
+# if the full border is included (red/green tiles), the area must valid
+def verifyArea(tile1: tuple, tile2: tuple):
+    x1, y1 = convertTile(tile1)
+    x2, y2 = convertTile(tile2)
+    if not checkPath(x1, y1, x2, y2):
+        return False
+    if not checkPath(x2, y2, x1, y1):
+        return False
     return True
 
 
@@ -95,5 +95,3 @@ for i in range(len(tiles) - 1):
             max_area = area
 
 print(f"Part 2: {max_area}")  # 1452422268
-
-# %%
